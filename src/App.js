ce598a58262c6f4ba47145f34c5c4e834721e39d
duckman-97 <72430680+duckman-97@users.noreply.github.com>
2022-09-23 12:01:22 +0900

    testcommit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import {AppBar,Toolbar,Typography} from '@mui/material'
+import Carlist from './components/Carlist'
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar position ="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Carshiop
+          </Typography>
+        </Toolbar>
+
+       </AppBar>
+   <Carlist />
+    </div>
+  )
+}
+
+export default App;
